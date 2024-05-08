e1ea1a773de057e3ab2cbdb3b38c301e7ce0354f
Joaquin Gonzalez <joagonzalez@gmail.com>
2024-05-08 20:55:38 -0300

    #47 add tests fixes qa

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,11 +3,13 @@ Tests for math functions.
 """
 import pytest
 
+from src.exceptions import (
+    AdditionError,
+    DivisionError,
+    MultiplicationError,
+    SubstractionError,
+)
 from src.services.calculator import Calculator
-from src.exceptions import AdditionError, \
-                            SubstractionError, \
-                            MultiplicationError, \
-                            DivisionError
 
 
 def test_suma_1():
@@ -36,17 +38,19 @@ def test_suma_4():
     Test 4 for suma()
     """
     assert Calculator.suma(0, 0) == 0
-    
+
+
 def test_suma_5():
     """
     Test 5 for suma()
     """
     with pytest.raises(AdditionError):
-        Calculator.suma('1', 2)
+        Calculator.suma("1", 2)
     with pytest.raises(AdditionError):
-        Calculator.suma(2, '1')
+        Calculator.suma(2, "1")
     with pytest.raises(AdditionError):
-        Calculator.suma('1', '2') 
+        Calculator.suma("1", "2")
+
 
 def test_resta_1():
     """
@@ -75,16 +79,18 @@ def test_resta_4():
     """
     assert Calculator.resta(0, 0) == 0
 
+
 def test_resta_5():
     """
     Test 5 for resta()
     """
     with pytest.raises(SubstractionError):
-        Calculator.resta('1', 2)
+        Calculator.resta("1", 2)
     with pytest.raises(SubstractionError):
-        Calculator.resta(2, '1')
+        Calculator.resta(2, "1")
     with pytest.raises(SubstractionError):
-        Calculator.resta('1', '2') 
+        Calculator.resta("1", "2")
+
 
 def test_multiplicacion_1():
     """
@@ -113,16 +119,17 @@ def test_multiplicacion_4():
     """
     assert Calculator.multiplicacion(-3, -3) == 9
 
+
 def test_multiplicacion_5():
     """
     Test 5 for multiplicacion()
     """
     with pytest.raises(MultiplicationError):
-        Calculator.multiplicacion('1', 2)
+        Calculator.multiplicacion("1", 2)
     with pytest.raises(MultiplicationError):
-        Calculator.multiplicacion(2, '1')
+        Calculator.multiplicacion(2, "1")
     with pytest.raises(MultiplicationError):
-        Calculator.multiplicacion('1', '2') 
+        Calculator.multiplicacion("1", "2")
 
 
 def test_division_1():
@@ -154,13 +161,14 @@ def test_division_4():
     """
     assert Calculator.division(-20, -5) == 4
 
+
 def test_division_5():
     """
     Test 5 for division()
     """
     with pytest.raises(DivisionError):
-        Calculator.division('1', 2)
+        Calculator.division("1", 2)
     with pytest.raises(DivisionError):
-        Calculator.division(2, '1')
+        Calculator.division(2, "1")
     with pytest.raises(DivisionError):
-        Calculator.division('1', '2') 
+        Calculator.division("1", "2")
